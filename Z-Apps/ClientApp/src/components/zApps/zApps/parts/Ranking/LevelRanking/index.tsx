7c9119dbbd750dcda03cc8b56e5a2cbe57aa92d8
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-10 12:08:24 +0900

    make top user ranking

--- Z-Apps/ClientApp/src/components/zApps/zApps/parts/Ranking/LevelRanking/index.tsx
+++ Z-Apps/ClientApp/src/components/zApps/zApps/parts/Ranking/LevelRanking/index.tsx
@@ -15,68 +15,118 @@ export function LevelRanking({ screenWidth }: { screenWidth: number }) {
 
     const isWide = screenWidth > 767;
 
+    const { topUsers, normalUsers } = users.reduce(
+        ({ topUsers, normalUsers }, user, i) => {
+            if (i < 3) {
+                return { topUsers: [...topUsers, user], normalUsers };
+            }
+            return { topUsers, normalUsers: [...normalUsers, user] };
+        },
+        {
+            topUsers: [] as UserForRanking[],
+            normalUsers: [] as UserForRanking[],
+        }
+    );
+
     return (
-        <Card className={spaceBetween("large", c.card)}>
-            <BasicRanking users={users} />
-        </Card>
+        <div
+            style={{
+                display: "flex",
+                flexDirection: isWide ? "row" : "column",
+            }}
+        >
+            <Card className={spaceBetween("large", c.card)}>
+                {topUsers.map((user, i) => (
+                    <BasicRankingRecord
+                        user={user}
+                        rank={i + 1}
+                        key={user.userId}
+                    />
+                ))}
+            </Card>
+            <Card
+                className={spaceBetween(
+                    "large",
+                    c.card,
+                    isWide ? c.marginLeft5 : null
+                )}
+            >
+                {normalUsers.map((user, i) => (
+                    <BasicRankingRecord
+                        user={user}
+                        rank={i + 4}
+                        key={user.userId}
+                    />
+                ))}
+            </Card>
+        </div>
     );
 }
 
 const useStyles = makeStyles(theme => ({
-    card: { marginTop: 10, backgroundColor: theme.palette.grey[100] },
+    card: {
+        marginTop: 10,
+        backgroundColor: theme.palette.grey[100],
+        height: 335,
+        overflow: "hidden",
+        flex: 1,
+    },
+    marginLeft5: { marginLeft: 5 },
 }));
 
-function BasicRanking({ users }: { users: UserForRanking[] }) {
+function BasicRankingRecord({
+    user,
+    rank,
+}: {
+    user: UserForRanking;
+    rank: number;
+}) {
     return (
-        <>
-            {users.map((u, i) => (
-                <Card
-                    key={u.userId}
-                    style={{
-                        margin: 5,
-                        display: "flex",
-                        padding: 5,
-                    }}
-                >
-                    <div
+        <Card
+            key={user.userId}
+            style={{
+                margin: 5,
+                display: "flex",
+                padding: 5,
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    marginLeft: 10,
+                    marginRight: 20,
+                }}
+            >
+                <div style={{ marginRight: 10 }}>{rank}.</div>
+                <Avatar>
+                    <img
+                        src={
+                            "https://lingualninja.blob.core.windows.net/lingual-storage/articles/_authors/1.jpg"
+                        }
                         style={{
-                            display: "flex",
-                            alignItems: "center",
-                            marginLeft: 10,
-                            marginRight: 20,
+                            width: 40,
+                            height: 40,
+                            objectFit: "cover",
+                            objectPosition: "50% 50%",
                         }}
-                    >
-                        <div style={{ marginRight: 10 }}>{i + 1}.</div>
-                        <Avatar>
-                            <img
-                                src={
-                                    "https://lingualninja.blob.core.windows.net/lingual-storage/articles/_authors/1.jpg"
-                                }
-                                style={{
-                                    width: 40,
-                                    height: 40,
-                                    objectFit: "cover",
-                                    objectPosition: "50% 50%",
-                                }}
-                                alt={u.name}
-                                title={u.name}
-                            />
-                        </Avatar>
-                    </div>
-                    <div
-                        style={{
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "space-around",
-                            width: "100%",
-                        }}
-                    >
-                        <div style={{ flex: 1 }}>{u.name}</div>
-                        <div style={{ flex: 1 }}>Lv. {u.level}</div>
-                    </div>
-                </Card>
-            ))}
-        </>
+                        alt={user.name}
+                        title={user.name}
+                    />
+                </Avatar>
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                    width: "100%",
+                }}
+            >
+                <div style={{ flex: 1 }}>{user.name}</div>
+                <div style={{ flex: 1 }}>Lv. {user.level}</div>
+            </div>
+        </Card>
     );
 }
 
@@ -96,5 +146,15 @@ async function fetchUsersForRanking(): Promise<UserForRanking[]> {
         },
         { userId: 1, name: "TJ", level: 3, xp: 300, avatarPath: "" },
         { userId: 35, name: "AJ", level: 2, xp: 120, avatarPath: "" },
+        { userId: 132, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 133, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 134, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 135, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 136, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 137, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 138, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 139, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 140, name: "Atom", level: 2, xp: 110, avatarPath: "" },
+        { userId: 141, name: "Atom", level: 2, xp: 110, avatarPath: "" },
     ];
 }
