d2c653df27e783702e2b6bbb6f533983154b7d32
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-15 23:52:27 +0900

    use className for smartphone top ranking

--- Z-Apps/ClientApp/src/components/zApps/zApps/parts/Ranking/LevelRanking/TopRankingRecord/TopRankingRecordSp.tsx
+++ Z-Apps/ClientApp/src/components/zApps/zApps/parts/Ranking/LevelRanking/TopRankingRecord/TopRankingRecordSp.tsx
@@ -1,6 +1,6 @@
 import Card from "@material-ui/core/Card";
+import { makeStyles } from "@material-ui/core/styles";
 import { appsPublicImg } from "../../../../../../../common/consts";
-import { theme } from "../../../../../Layout";
 import { UserForRanking } from "../types";
 import { UserAvatar } from "../UserAvatar";
 
@@ -11,52 +11,20 @@ export function TopRankingRecordSp({
     user: UserForRanking;
     rank: number;
 }) {
+    const c = useStyles();
+
     return (
-        <Card
-            style={{
-                margin: 5,
-                display: "flex",
-                padding: 5,
-                height: 105,
-            }}
-        >
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    marginRight: 10,
-                }}
-            >
-                <Card
-                    style={{
-                        height: 95,
-                        width: 95,
-                        backgroundColor: theme.palette.grey[200],
-                        marginLeft: 0,
-                        marginRight: 15,
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                    }}
-                >
+        <Card className={c.containerCard}>
+            <div className={c.trophyAndAvatar}>
+                <Card className={c.trophyContainer}>
                     <img
                         src={`${appsPublicImg}user_ranking/${rank}.png`}
-                        style={{ width: 85, height: 85 }}
+                        className={c.trophyImg}
                     />
                 </Card>
                 <UserAvatar user={user} rank={rank} size={50} />
             </div>
-            <div
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "100%",
-                    fontSize: "1.3rem",
-                    wordBreak: "break-all",
-                    marginRight: 5,
-                }}
-            >
+            <div className={c.nameAndLevelContainer}>
                 <div>
                     <div>{user.name}</div>
                     <div>Lv. {user.level}</div>
@@ -65,3 +33,37 @@ export function TopRankingRecordSp({
         </Card>
     );
 }
+
+const useStyles = makeStyles(theme => ({
+    containerCard: {
+        margin: 5,
+        display: "flex",
+        padding: 5,
+        height: 105,
+    },
+    trophyAndAvatar: {
+        display: "flex",
+        alignItems: "center",
+        marginRight: 10,
+    },
+    trophyContainer: {
+        height: 95,
+        width: 95,
+        backgroundColor: theme.palette.grey[200],
+        marginLeft: 0,
+        marginRight: 15,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    trophyImg: { width: 85, height: 85 },
+    nameAndLevelContainer: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        fontSize: "1.3rem",
+        wordBreak: "break-all",
+        marginRight: 5,
+    },
+}));
