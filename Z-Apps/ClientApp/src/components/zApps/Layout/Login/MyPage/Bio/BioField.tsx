91ee703f78aef07e3cadc8982d0fd0eac16d14c7
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-16 17:43:13 +0900

    bio validation

--- Z-Apps/ClientApp/src/components/zApps/Layout/Login/MyPage/Bio/BioField.tsx
+++ Z-Apps/ClientApp/src/components/zApps/Layout/Login/MyPage/Bio/BioField.tsx
@@ -1,6 +1,6 @@
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import PencilIcon from "@material-ui/icons/Create";
 import { useEffect, useState } from "react";
@@ -8,7 +8,6 @@ import { User } from "../../../../../../common/hooks/useUser";
 import { Markdown } from "../../../../../shared/Markdown";
 
 export function BioField({ user }: { user: User }) {
-    const c = useBioAreaStyles();
     const [editMode, setEditMode] = useState(false);
     const [bio, setBio] = useState(user.bio);
 
@@ -16,9 +15,16 @@ export function BioField({ user }: { user: User }) {
         setBio(user.bio);
     }, [user.bio]);
 
+    const error =
+        bio.length > 999
+            ? "Your bio must be less than 1000 characters."
+            : undefined;
+
+    const c = useBioAreaStyles({ isError: !!error });
+
     if (editMode) {
         return (
-            <div style={{ width: "100%", marginTop: 25, position: "relative" }}>
+            <div className={c.editContainer}>
                 <TextField
                     label="Bio"
                     variant="outlined"
@@ -33,6 +39,12 @@ export function BioField({ user }: { user: User }) {
                             paddingBottom: 20,
                         },
                     }}
+                    autoFocus
+                    helperText={
+                        error ? (
+                            <div className={c.errorText}>{error}</div>
+                        ) : undefined
+                    }
                 />
                 <Button
                     variant="contained"
@@ -41,6 +53,7 @@ export function BioField({ user }: { user: User }) {
                         setEditMode(false);
                     }}
                     color="primary"
+                    disabled={!!error}
                 >
                     Save
                 </Button>
@@ -49,8 +62,13 @@ export function BioField({ user }: { user: User }) {
     }
 
     return (
-        <div style={{ width: "100%", marginTop: 25, position: "relative" }}>
-            <Card style={{ padding: 15 }}>
+        <div className={c.previewContainer}>
+            <Card
+                className={c.markdownContainerCard}
+                onClick={() => {
+                    setEditMode(true);
+                }}
+            >
                 <Markdown source={bio} />
             </Card>
             <Button
@@ -60,34 +78,45 @@ export function BioField({ user }: { user: User }) {
                     setEditMode(true);
                 }}
             >
-                <PencilIcon style={{ width: 20, height: 20 }} />
+                <PencilIcon className={c.pencilIcon} />
             </Button>
         </div>
     );
 }
-const useBioAreaStyles = makeStyles(({ palette }) => ({
-    editButton: {
-        backgroundColor: palette.grey[800],
-        color: "white",
-        maxWidth: 30,
-        maxHeight: 30,
-        minWidth: 30,
-        minHeight: 30,
-        position: "absolute",
-        bottom: 5,
-        right: 5,
-        transition: "all 200ms",
-        "&:hover": {
-            backgroundColor: palette.grey[600],
+const useBioAreaStyles = makeStyles<Theme, { isError: boolean }>(
+    ({ palette }) => ({
+        editContainer: { width: "100%", marginTop: 25, position: "relative" },
+        errorText: { color: "red", height: 20 },
+        saveButton: ({ isError }) => ({
+            position: "absolute",
+            bottom: isError ? 30 : 5,
+            right: 5,
+            maxWidth: 30,
+            maxHeight: 30,
+            transition: "all 200ms",
+        }),
+        pencilIcon: { width: 20, height: 20 },
+        previewContainer: {
+            width: "100%",
+            marginTop: 25,
+            position: "relative",
         },
-    },
-    saveButton: {
-        position: "absolute",
-        bottom: 5,
-        right: 5,
-        maxWidth: 30,
-        maxHeight: 30,
-        transition: "all 200ms",
-    },
-}));
+        markdownContainerCard: { padding: 15, cursor: "pointer" },
+        editButton: {
+            backgroundColor: palette.grey[800],
+            color: "white",
+            maxWidth: 30,
+            maxHeight: 30,
+            minWidth: 30,
+            minHeight: 30,
+            position: "absolute",
+            bottom: 5,
+            right: 5,
+            transition: "all 200ms",
+            "&:hover": {
+                backgroundColor: palette.grey[600],
+            },
+        },
+    })
+);
 
