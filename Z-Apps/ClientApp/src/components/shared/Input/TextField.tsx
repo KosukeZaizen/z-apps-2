98131531a5934daac4bd0bbc591e8c25b9733656
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-18 13:16:42 +0900

    initial input style for TextField

--- Z-Apps/ClientApp/src/components/shared/Input/TextField.tsx
+++ Z-Apps/ClientApp/src/components/shared/Input/TextField.tsx
@@ -1,4 +1,6 @@
+import { InputBaseComponentProps } from "@material-ui/core/InputBase";
 import MuiTextField, { TextFieldProps } from "@material-ui/core/TextField";
+import { CSSProperties, useState } from "react";
 
 export function TextField({
     onMouseDown,
@@ -6,6 +8,10 @@ export function TextField({
     inputProps,
     ...rest
 }: TextFieldProps) {
+    const [inputStyle, setInputStyle] = useState<CSSProperties | undefined>(
+        getInitialInputStyle(inputProps, rest.autoFocus)
+    );
+
     return (
         <MuiTextField
             {...rest}
@@ -16,22 +22,28 @@ export function TextField({
                  */
                 (
                     ev.target as HTMLInputElement | HTMLTextAreaElement
-                ).style.fontSize = "16px";
+                ).style.fontSize = "16px"; // Modify the real DOM directory before iPhone detects the font-size
+
+                setInputStyle({ ...inputProps?.style, fontSize: 16 }); // Virtual DOM will also be changed
+
                 onMouseDown?.(ev);
             }}
             onFocus={ev => {
-                ev.target.style.fontSize = "";
+                setInputStyle({ ...inputProps?.style });
+
                 onFocus?.(ev);
             }}
-            inputProps={
-                rest.autoFocus
-                    ? {
-                          ...inputProps,
-                          style: { ...inputProps?.style, fontSize: 16 },
-                      }
-                    : inputProps
-            }
+            inputProps={{ ...inputProps, style: inputStyle }}
         />
     );
 }
 
+function getInitialInputStyle(
+    inputProps: InputBaseComponentProps | undefined,
+    autoFocus?: boolean
+): CSSProperties | undefined {
+    if (autoFocus) {
+        return { ...inputProps?.style, fontSize: 16 };
+    }
+    return inputProps?.style;
+}
