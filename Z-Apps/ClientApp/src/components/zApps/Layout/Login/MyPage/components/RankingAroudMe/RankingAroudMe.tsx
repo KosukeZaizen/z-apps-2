850c58f93556daea04a6fbe7c78dc8d731ac1d12
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-20 11:13:17 +0900

    use className for RankingAroundMe

--- Z-Apps/ClientApp/src/components/zApps/Layout/Login/MyPage/components/RankingAroudMe/RankingAroudMe.tsx
+++ Z-Apps/ClientApp/src/components/zApps/Layout/Login/MyPage/components/RankingAroudMe/RankingAroudMe.tsx
@@ -1,3 +1,4 @@
+import { makeStyles, Theme } from "@material-ui/core";
 import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import { useEffect, useState } from "react";
 import { useScreenSize } from "../../../../../../../common/hooks/useScreenSize";
@@ -13,17 +14,13 @@ export function RankingAroundMe({ user: player }: { user: User }) {
     const { screenWidth } = useScreenSize();
 
     useEffect(() => {
-        if (open) {
-            fetchUsersAroundMyRank(player.userId).then(({ users, myRank }) => {
-                setUsers(users);
-                setMyRank(myRank);
-            });
-            return;
-        }
-        fetchMyRank(player.userId).then(myRank => {
+        fetchUsersAroundMyRank(player.userId).then(({ users, myRank }) => {
+            setUsers(users);
             setMyRank(myRank);
         });
-    }, [player, open]);
+    }, [player]);
+
+    const c = useStyles({ screenWidth });
 
     return (
         <OpenableCard
@@ -35,39 +32,32 @@ export function RankingAroundMe({ user: player }: { user: User }) {
             alwaysShowIcon
             alwaysShowTitle
         >
-            {users.length > 0 && (
-                <div
-                    style={{
-                        width: "100%",
-                        display: "flex",
-                        justifyContent: "center",
-                    }}
-                >
-                    <div
-                        style={{
-                            width: "calc(100% - 5px)",
-                            marginBottom:
-                                screenWidth > 480
-                                    ? undefined
-                                    : Math.max(-35, -(480 - screenWidth) / 2),
-                        }}
-                    >
-                        <BasicRanking users={users} />
-                    </div>
+            <div className={c.container}>
+                <div className={c.rankingWrapper}>
+                    <BasicRanking users={users} />
                 </div>
-            )}
+            </div>
         </OpenableCard>
     );
 }
+const useStyles = makeStyles<Theme, { screenWidth: number }>({
+    container: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+    },
+    rankingWrapper: ({ screenWidth }) => ({
+        width: "calc(100% - 5px)",
+        marginBottom:
+            screenWidth > 480
+                ? undefined
+                : Math.max(-35, -(480 - screenWidth) / 2),
+    }),
+});
 
 async function fetchUsersAroundMyRank(
     userId: number
 ): Promise<{ users: UserForRanking[]; myRank: number }> {
     const res = await fetch(`api/User/GetUsersAroundMyRank?userId=${userId}`);
-    return res.json();
-}
-
-async function fetchMyRank(userId: number): Promise<number> {
-    const res = await fetch(`api/User/GetMyRank?userId=${userId}`);
     return res.json();
 }
