22a76ae0200f493edaa3c30bab0af134ad379aeb
Kosuke_Zaizen <kosukezaizen@yahoo.co.jp>
2022-07-21 19:56:20 +0900

    use className for ShurikenProgress

--- Z-Apps/ClientApp/src/components/shared/Animations/ShurikenProgress/index.tsx
+++ Z-Apps/ClientApp/src/components/shared/Animations/ShurikenProgress/index.tsx
@@ -1,47 +1,61 @@
-import * as React from "react";
+import { Theme } from "@material-ui/core";
+import { BaseCSSProperties, makeStyles } from "@material-ui/styles";
 import { appsPublicImg } from "../../../../common/consts";
+import { spaceBetween } from "../../../../common/util/Array/spaceBetween";
 import "./animation.css";
 
 const shuriken = appsPublicImg + "shuriken.png";
 
 interface Props {
     size?: string | number;
-    style?: React.CSSProperties;
+    style?: BaseCSSProperties;
 }
 export default function ShurikenProgress({ size, style }: Props) {
+    const c = useShurikenProgressStyles({ size, style });
     return (
-        <div
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                ...style,
-            }}
-            className="center"
-        >
+        <div className={spaceBetween("center", c.container)}>
             <img
                 src={shuriken}
                 alt="shuriken"
-                className="ShurikenProgress"
-                style={{ width: size, height: size }}
+                className={spaceBetween("ShurikenProgress", c.img)}
             />
         </div>
     );
 }
+const useShurikenProgressStyles = makeStyles<
+    Theme,
+    {
+        size?: string | number;
+        style?: BaseCSSProperties;
+    }
+>({
+    container: ({ style }) => ({
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        ...style,
+    }),
+    img: ({ size }) => ({ width: size, height: size }),
+});
 
 export function FullScreenShurikenProgress({ style, size }: Props) {
+    const c = useFullScreenShurikenProgressStyles({ style });
     return (
-        <div
-            style={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                ...style,
-            }}
-        >
+        <div className={c.container}>
             <ShurikenProgress size={size || "20%"} />
         </div>
     );
 }
+const useFullScreenShurikenProgressStyles = makeStyles<
+    Theme,
+    { style?: BaseCSSProperties }
+>({
+    container: ({ style }) => ({
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        ...style,
+    }),
+});
